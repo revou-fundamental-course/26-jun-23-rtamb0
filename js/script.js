b274954c3776e1c14e92ef879e1c9eeb612dafb0
s0ycloud <135024553+s0ycloud@users.noreply.github.com>
2023-07-05 18:43:38 +0700

    Put error messages elements before the label

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
 const name = document.querySelector('.name-input');
 const nameInput = document.querySelector('#name');
 const wrongName = document.createElement('div');
-name.insertBefore()
+name.insertBefore(wrongName, nameInput);
 
 const email = document.querySelector('.email-input');
 const emailInput = document.querySelector('#email');
 const wrongEmail = document.createElement('div');
-
+email.insertBefore(wrongEmail, emailInput);
 
 const phone = document.querySelector('.phone-input');
 const phoneInput = document.querySelector('#phone');
 const wrongPhone = document.createElement('div');
-
+phone.insertBefore(wrongPhone, phoneInput);
 
 const interest = document.querySelector('.interest-options');
 const interestSelect = document.querySelector('#interest');
 const noInterest = document.createElement('div');
-
+interest.insertBefore(noInterest, interestSelect);
 
 const message = document.querySelector('.message-input');
 const messageInput = document.querySelector('#message');
 const wrongMessage = document.createElement('div');
+message.insertBefore(wrongMessage, messageInput);
