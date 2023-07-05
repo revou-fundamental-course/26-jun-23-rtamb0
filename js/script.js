46d321fe4ffc4825a51a452ced380dfd077a5fae
s0ycloud <135024553+s0ycloud@users.noreply.github.com>
2023-07-05 17:43:08 +0700

    Add variables for error messages on input

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-const name = document.querySelector('#name');
-const email = document.querySelector('#email');
-const phone = document.querySelector('#phone');
-const interest = document.querySelector('#interest');
-const message = document.querySelector('#message');
+const name = document.querySelector('.name-input');
+const nameInput = document.querySelector('#name');
+const wrongName = document.createElement('div');
+name.insertBefore()
+
+const email = document.querySelector('.email-input');
+const emailInput = document.querySelector('#email');
+const wrongEmail = document.createElement('div');
+
+
+const phone = document.querySelector('.phone-input');
+const phoneInput = document.querySelector('#phone');
+const wrongPhone = document.createElement('div');
+
+
+const interest = document.querySelector('.interest-options');
+const interestSelect = document.querySelector('#interest');
+const noInterest = document.createElement('div');
+
+
+const message = document.querySelector('.message-input');
+const messageInput = document.querySelector('#message');
+const wrongMessage = document.createElement('div');
