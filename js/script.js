6eef8596716920d67ba155cc8d230d3cb0a659d3
s0ycloud <135024553+s0ycloud@users.noreply.github.com>
2023-07-05 17:15:01 +0700

    Add variables containing the label elements

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const phone = document.querySelector('#phone');
+const interest = document.querySelector('#interest');
+const message = document.querySelector('#message');
